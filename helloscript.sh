a782028632d8bd5c65209b9d3352b6968839ab63
Dennis Simpson <dennis@zonzorp.net>
2015-09-22 15:51:31 -0400

    Fixed helloscript.sh and made both executable

--- helloscript.sh
+++ helloscript.sh
@@ -4,4 +4,4 @@
 # display the string Hello World
 
 # this is a silly way of creating the output string
-echo -n "helb wolb" | sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" | tr "h" "H" | tr "w" "W"
+echo -n "helb wold" | sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" | tr "h" "H" | tr "w" "W"
