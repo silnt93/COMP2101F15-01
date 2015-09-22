eebc4ef08ceffe0749f45641618349ea7e137e99
Dennis Simpson <dennis@zonzorp.net>
2015-09-22 19:11:55 +0000

    Added the helloscript.sh file to the repository

---
+++ helloscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# helloscript.sh
+# display the string Hello World
+
+# this is a silly way of creating the output string
+echo -n "helb wolb" | sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" | tr "h" "H" | tr "w" "W"
