d01f7cf4fd76654cb015a3d553767f1d6f8c8b66
Dennis Simpson <dennis@zonzorp.net>
2015-09-29 21:12:14 +0000

    You never loved me

---
+++ welcome.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this is my welcome script to properly address me
+
+mytitle="Supreme Commander"
+myname="Dennis"
+hostname=`hostname`
+weekday=$(date +%A)
+echo "Welcome to planet $hostname, $mytitle $myname!"
+echo "Today is $weekday."
