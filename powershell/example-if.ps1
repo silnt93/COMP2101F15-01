4d6615cfd11d55b2fac3fb9d87b2722deebb5f55
Dennis Simpson <dennis@zonzorp.net>
2015-12-01 15:23:58 -0500

    Create example-if.ps1

---
+++ powershell/example-if.ps1
@@ -0,0 +1,15 @@
+$gob = "mygob"
+$smacked = $true
+
+$LiberalSeats = 184
+$PCSeats = 5
+
+if ($gob -eq $smacked) {
+    "Gob was smacked"
+}
+elseif ($LiberalSeats -gt $PCSeats) {
+    "Cons are mad, bro"
+ }
+ else {
+    "No gob smacking around these parts, mate"
+}
